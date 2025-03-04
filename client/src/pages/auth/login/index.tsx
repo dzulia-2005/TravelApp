2d4d646914bb7e1508b12d7c1eae270502d8a93e
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-03-04 16:33:39 +0400

    added reacthookform & zodResolver

--- client/src/pages/auth/login/index.tsx
+++ client/src/pages/auth/login/index.tsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import { AntDesignOutlined, UserOutlined } from '@ant-design/icons';
 import { Input , Space,  Button} from 'antd';
+import { Loginpayload } from '../../../api/auth/index.types';
+import { useNavigate } from 'react-router-dom';
+import {LoginSchema} from "./schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useLogin } from '../../../react-query/mutation/auth';
+import { SigninSuccess } from './utils';
+import { queryClient } from '../../../main';
+import { Controller, useForm } from "react-hook-form";
 
+
+type loginFormValues = Loginpayload["payload"];
+
+const LoginFormDefaultValues:loginFormValues = {
+  email:"",
+  password:""
+}
 
 const Login:React.FC = () => {
+  const navigate = useNavigate();
+
+  const {
+    control,
+    handleSubmit,
+    formState:{errors}
+  }=useForm<loginFormValues>({
+    defaultValues:LoginFormDefaultValues,
+    resolver:zodResolver(LoginSchema)
+  })
+
+  const {mutate:handleLogin} = useLogin();
+
+  const onSubmit = (Loginpayload:loginFormValues) => {
+      handleLogin({payload:Loginpayload},{
+          onSuccess:(res)=>{
+            SigninSuccess({
+              accessToken:res.accessToken,
+              refreshToken:res.refreshToken,
+            })
+            navigate("/")
+            queryClient.invalidateQueries({queryKey:["me"]});
+          },
+          onError:()=>{
+            alert("login Failed")
+          }
+      })
+  }
 
   return (
     <div className='flex justify-center'>
@@ -11,9 +54,45 @@ const Login:React.FC = () => {
                 <div className='text-center text-2xl py-12'>Sign In</div>
                 <div className='text-center'>
                     <Space direction="vertical">
-                        <Input className='mb-2' size='large'  placeholder="Email" prefix={<UserOutlined />} />
-                        <Input.Password size='large' placeholder="password" />
-                        <Button className='mt-5' type="primary" size="large" icon={<AntDesignOutlined />}>
+                        <Controller
+                          control={control}
+                          name="email"
+                          render={({field:{onChange,value}})=>{
+                              return <Input 
+                                className='mb-2' 
+                                onChange={onChange}
+                                value={value}
+                                size='large'  
+                                placeholder="Email"
+                                prefix={<UserOutlined />} 
+                              />
+                          }}
+                        />
+                        {errors.email && (
+                           <p className="text-red-600">{errors.email.message}</p>
+                        )}
+                       <Controller
+                          control={control}
+                          name="password"
+                          render={({field:{onChange,value}})=>{
+                            return  <Input.Password 
+                                      size='large' 
+                                      placeholder="password" 
+                                      onChange={onChange}
+                                      value={value}
+                                      />
+                          }}  
+                        />
+                        {errors.password && (
+                           <p className="text-red-600">{errors.password.message}</p>
+                        )}
+                        <Button 
+                          className='mt-5' 
+                          type="primary" 
+                          size="large" 
+                          icon={<AntDesignOutlined />}
+                          onClick={handleSubmit(onSubmit)}
+                        >
                             Log In
                         </Button>
                     </Space>
