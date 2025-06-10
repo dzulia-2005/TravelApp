123f6a3d020eb0bd0815e42a3e76ae365aedbf69
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-06-10 14:35:47 +0400

    added search input on home page

--- client/src/pages/home/view/index.tsx
+++ client/src/pages/home/view/index.tsx
@@ -1,15 +1,28 @@
-import {Card} from "antd";
+import {Card, Input} from "antd";
 import {useGetAllCardsQuery} from "../../../features/card/cardApi.ts";
+import { useState } from "react";
 
 
 const Home = () => {
+    const [searchTerm,setSearchTerm] = useState("");
     const {data} = useGetAllCardsQuery();
+
+    const filterCards = data?.filter((card)=>
+        card.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
   return (
     <>
+            <div className="my-3">
+                <Input 
+                    placeholder="Search" 
+                    value={searchTerm} 
+                    onChange={(e)=>setSearchTerm(e.target.value)}
+                />
+            </div>
              <div className="grid grid-cols-4 gap-4">
                  {
-                     data?.map((card)=>(
+                     filterCards?.map((card)=>(
                          <Card
                              hoverable
                              style={{ width: 240 }}
